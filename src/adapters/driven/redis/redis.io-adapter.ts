00bce28686574284f6476a8399e6acab6b872136
racgoo <lhsung98@naver.com>
2024-11-07 02:13:31 +0900

    fix(redis): add password env

--- src/adapters/driven/redis/redis.io-adapter.ts
+++ src/adapters/driven/redis/redis.io-adapter.ts
@@ -3,13 +3,27 @@ import { ServerOptions } from 'socket.io';
 import { createAdapter } from '@socket.io/redis-adapter';
 import { createClient } from 'redis';
 import { Injectable } from '@nestjs/common';
+import { EnvService } from 'src/shared/modules/env/env.service';
 
 @Injectable()
 export class RedisIoAdapter extends IoAdapter {
   private adapterConstructor: ReturnType<typeof createAdapter>;
-
+  constructor(private readonly envService: EnvService) {
+    super();
+  }
   async connectToRedis(): Promise<void> {
-    const pubClient = createClient({ url: `redis://localhost:6379` });
+    let pubClient;
+    if (this.envService.get('NODE_ENV') === 'production') {
+      pubClient = createClient({
+        url: this.envService.get('REDIS_URL'),
+        password: this.envService.get('REDIS_PASSWORD'),
+      });
+    } else {
+      pubClient = createClient({
+        url: this.envService.get('REDIS_URL'),
+      });
+    }
+
     const subClient = pubClient.duplicate();
     await Promise.all([pubClient.connect(), subClient.connect()]);
     this.adapterConstructor = createAdapter(pubClient, subClient);
